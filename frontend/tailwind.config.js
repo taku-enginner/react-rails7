580191049195280043062bf1644c54a1ccbceae0
taka4655 <taku.enginner@gmail.com>
2025-01-30 20:26:43 +0900

    corsなどの設定、tailwindcss、daisyui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // すべてのJS、JSX、TS、TSXファイルをスキャン
+  ],
+  future: {},
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake', 'aqua', 'pastel'],
+  },
+};
